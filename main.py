b3633c9e3d83802859d0a8572881f5f3f7f85c95
Tom Payne <tom@agilepond.com>
2025-07-10 18:47:17 -0700

    Refactor main.py to integrate MCP client session handling and enhance functionality. Added support for processing messages with a React agent and included a sample calculation query.

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import os
 
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
-
 from langchain_openai import ChatOpenAI
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import HumanMessage
 
 load_dotenv()
 
@@ -21,7 +21,19 @@ stdio_server_params = StdioServerParameters(
 
 
 async def main():
-    print("Hello from mcp-crash-course!")
+    async with stdio_client(stdio_server_params) as (read, write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print ("session initialized")
+            print("Hello from mcp-crash-course!")
+            tools = await load_mcp_tools(session)
+            #print(tools)
+
+            agent = create_react_agent(llm, tools)
+            result = await agent.ainvoke({"messages": [HumanMessage(content="What is 53 + 4 * 23?")]})
+            print(result["messages"][-1].content)
+
+
 
 
 if __name__ == "__main__":
